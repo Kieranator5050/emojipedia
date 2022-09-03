1bcf3cd340dade97ef37b6c7a35f6d47377524f2
Kieran Jaggernauth <thejugg63@gmail.com>
2022-09-03 13:38:37 -0500

    Updates

---
+++ precache-manifest.9142a869538cecb3118842c9b0566fc2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ca416bd1349cedcfbbdc29d2992a35d",
+    "url": "/emojipedia/index.html"
+  },
+  {
+    "revision": "c88de36f633a6c4439b0",
+    "url": "/emojipedia/static/js/2.f262ee4d.chunk.js"
+  },
+  {
+    "revision": "6c99a11c853b0790fdf0",
+    "url": "/emojipedia/static/js/main.96d1142e.chunk.js"
+  },
+  {
+    "revision": "3ad02f26d7c433e7efca",
+    "url": "/emojipedia/static/js/runtime-main.d1c65f91.js"
+  }
+]);
